0802544269e6cc0bc31dae4066cd601c7e39989f
HanzerT <42140998+HanzerT@users.noreply.github.com>
2018-08-06 13:06:32 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import DataCreator from './Components/DataCreator';
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <DataCreator/>
+      </div>
+    )
+  }
+}
+
+export default App
